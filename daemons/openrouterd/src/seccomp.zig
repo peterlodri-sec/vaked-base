a04a2915e345ac36cf4d9f334ac4130923dd27bb
Lodri Péter <267054113+peterlodri-sec@users.noreply.github.com>
2026-06-20 08:41:41 +0200

    fix(openrouterd): @intFromBool(1) to @intFromBool(true) for Zig 0.16

--- daemons/openrouterd/src/seccomp.zig
+++ daemons/openrouterd/src/seccomp.zig
@@ -5,7 +5,7 @@ const sock_filter = extern struct { code: u16, jt: u8, jf: u8, k: u32 };
 const ALLOWED = [_]u32{ 0, 1, 257, 3, 41, 42, 44, 45, 291, 233, 232, 9, 11, 10, 12, 60, 231, 318, 228, 202, 425, 426 };
 pub fn apply() void {
     if (builtin.os.tag != .linux) return;
-    _ = linux.prctl(@intFromEnum(linux.PR.SET_NO_NEW_PRIVS), @intFromBool(1), 0, 0, 0);
+    _ = linux.prctl(@intFromEnum(linux.PR.SET_NO_NEW_PRIVS), @intFromBool(true), 0, 0, 0);
     var filter: [ALLOWED.len + 2]sock_filter = undefined;
     inline for (ALLOWED, 0..) |syscall, idx| {
         filter[idx] = .{ .code = 0x15, .jt = @intCast(ALLOWED.len - idx), .jf = 1, .k = syscall };
